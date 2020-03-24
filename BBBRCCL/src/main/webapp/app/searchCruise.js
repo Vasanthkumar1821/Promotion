218281dc8729d5154ca4d6bb199ccf919a7b3f2c
HVasanth <HVasanth@host.docker.internal>
2020-03-24 10:09:24 -0400

    ini

--- BBBRCCL/src/main/webapp/app/searchCruise.js
+++ BBBRCCL/src/main/webapp/app/searchCruise.js
@@ -1,38 +1,72 @@
 var app = angular.module('searchCruise', []);
 
-app.controller('searchCruiseController', function($scope, $http, $window) {
+app
+		.controller(
+				'searchCruiseController',
+				function($scope, $http, $window) {
 
-	$scope.cruises = {
-		_id : "",
-		shipcode : "",
-		shipname : "",
-		sailingdate : "",
-		state : "",
-		destination : ""
-	}
-	$scope.searchCruise = function() {
-
-		$http(
-				{
-					method : 'GET',
-					url : 'http://localhost:8062/api/cruises/'
-							+ $scope.cruises.state + '/'
-							+ $scope.cruises.destination + '/'
-							+ $scope.cruises.sailingdate,
-					data : angular.toJson($scope.cruises),
-					headers : {
-						'Content-Type' : 'application/json'
+					$scope.cruises = {
+						_id : "",
+						shipcode : "",
+						shipname : "",
+						sailingdate : "",
+						state : "",
+						destination : ""
 					}
-				}).then(function(response) {
-			console.log("success data");
-			console.log(response.data);
-			$scope.cruises = null;
-			$scope.cru = response.data;
-			$scope.cru.sailingdate = new Date($scope.cru.sailingdate);
-			console.log($scope.cru);
-		});
-	}
-});
+
+					$scope.searchCruise = function() {
+
+						$http(
+								{
+									method : 'GET',
+									url : 'http://localhost:8062/api/cruises/'
+											+ $scope.cruises.state + '/'
+											+ $scope.cruises.destination + '/'
+											+ $scope.cruises.sailingdate,
+									data : angular.toJson($scope.cruises),
+									headers : {
+										'Content-Type' : 'application/json'
+									}
+								})
+								.then(
+										function(response) {
+											console.log("success data");
+
+											$scope.curPage = 1,
+													$scope.itemsPerPage = 3,
+													$scope.maxSize = 5;
+
+											this.items = itemsDetails;
+
+											$scope.numOfPages = function() {
+												return Math
+														.ceil(itemsDetails.length
+																/ $scope.itemsPerPage);
+
+											};
+
+											$scope
+													.$watch(
+															'curPage + numPerPage',
+															function() {
+																var begin = (($scope.curPage - 1) * $scope.itemsPerPage), end = begin
+																		+ $scope.itemsPerPage;
+
+																$scope.cru = response.data
+																		.slice(
+																				begin,
+																				end);
+															});
+
+											console.log(response.data);
+											$scope.cruises = null;
+											// $scope.cru = response.data;
+											$scope.cru.sailingdate = new Date(
+													$scope.cru.sailingdate);
+											console.log($scope.cru);
+										});
+					}
+				});
 
 app.controller('viewCruises', function($scope, $http) {
 	$scope.cruises = {
